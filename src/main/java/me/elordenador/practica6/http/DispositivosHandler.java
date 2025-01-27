1dd4367444512adb1502b271152df0ef8dc6eaa6
Daniel <danilacasito8@gmail.com>
2025-01-27 12:13:13 +0100

    Add Small HTTP Server

---
+++ src/main/java/me/elordenador/practica6/http/DispositivosHandler.java
@@ -0,0 +1,15 @@
+package me.elordenador.practica6.http;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class DispositivosHandler implements HttpHandler {
+    private String response;
+    public void handle(HttpExchange ex) {
+
+    }
+
+    public void generateHTMLRoot() {
+
+    }
+}
---
+++ src/main/java/me/elordenador/practica6/http/HResponder.java
@@ -0,0 +1,22 @@
+package me.elordenador.practica6.http;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HResponder {
+    private static HttpServer server;
+    public static void main(String[] args) throws IOException {
+        server = HttpServer.create(new InetSocketAddress(8000), 0);
+        createContext();
+
+        server.setExecutor(null);
+        server.start();
+    }
+
+    private static void createContext() {
+        server.createContext("/", new RootHandler());
+        server.createContext("/dispositivos", new DispositivosHandler());
+    }
+}
